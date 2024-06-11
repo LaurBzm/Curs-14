6d2f5182e866735d939daebcb6e2e0d64a1329e5
LaurBzm <165313394+LaurBzm@users.noreply.github.com>
2024-06-11 16:49:30 +0300

    first commit

--- src/main/java/org/example/Repository.java
+++ src/main/java/org/example/Repository.java
@@ -116,3 +116,19 @@ class MainRecursivity
         System.out.println("Suma digit este: "+res);
     }
 }
+class Fibonacci {
+    static int fib(int n)
+    {
+        if (n==0)
+            return 0;
+        else if(n==1 || n==2)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String args[])
+    {
+        int n = 9;
+        System.out.println(fib(n));
+    }
+}
